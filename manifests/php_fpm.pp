642739c004f623d2937b9f9b810a2ec070e2daa7
Jérôme Arzel <jerome.arzel@gnuside.com>
2013-10-01 14:50:47 +0200

    Adding php-mysql extension.

--- manifests/php_fpm.pp
+++ manifests/php_fpm.pp
@@ -2,6 +2,11 @@ class nginx::php_fpm {
   $sock_file = "/var/run/php5-fpm.sock"
 
   package {
+<<<<<<< HEAD
     "php5-fpm": ensure => installed;
+=======
+    "php5-fpm":   ensure => installed;
+    "php5-mysql": ensure => installed;
+>>>>>>> develop
   }
 }
